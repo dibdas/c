7b03156eb89aa7745faf184afdcce6ab477874fe
dibdas <dibyandu08das@gmail.com>
2021-08-16 23:57:05 +0530

    modified

--- src/containers/Meals.js
+++ src/containers/Meals.js
@@ -8,4 +8,64 @@ import Categories from '../components/Categories';
 
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
+import Meals from '../components/Meals';
+import { mealsByCategory } from '../API/api';
+import { stat } from 'fs';
+
+const sx = className.bind(grid)
+const Meals =({meals:{meals,status, error}, mealsbycategory,category,changecategory}) =>{
+    const { categorytype } =userParams()
+    useEffect(()=>{
+        changecategory(categorytype)
+        if(status === actionsType.IDLE_MEALS||categorytype){
+            mealsByCategory(categorytype || 'chicken')
+        }
+    },[category])
+    if(status === actionsType.ERROR_MEALS){
+        return(
+            <div>
+                Error:{error}
+                </div>
+
+        )
+    }
+    else {
+        return(
+            <div>
+                {meals && meals.map((meal)=> <Meal key={meal.idMeal} meal={meal} />)}
+            </div>
+        )
+    }
+}
+
+
+Meals.defaultProps = {
+    category: 'chicken',
+  };
+
+Meals.prototypes ={
+    meals:PropTypes.shape({
+        status: PropTypes.isRequired,
+        error: PropTypes.isRequired,
+        meals:  PropTypes.objectOf(PropTypes.string).isRequired,
+    }).isRequired,
+    category: PropTypes.string.isRequired,
+    changecategory: PropTypes.func.isRequired,
+    mealsbycategory: PropTypes.func.isRequired
+
+
+}
+
+const mapStateToProps = (state) => ({
+    meals: state.meals,
+    category: state.category,
+  });
+
+  const mapDispatchToProps = (dispatch)=>({
+      changecategory: (category) => dispatch(changecategory(category)),
+      mealsbycategory: (category) => dispatch(mealsByCategory(category))
+
+
+  })
+export default connect(mapStateToProps)(Categories);
 
