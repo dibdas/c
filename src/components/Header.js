acb2f0f6fb97022073819f6e507e60752b378ae1
dibdas <dibyandu08das@gmail.com>
2021-08-16 10:42:21 +0530

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import className from 'classnames';
+import { Link } from 'react-router-dom';
+import search from '../containers/Search';
+import styles from '../styles/Header.css';
+ const sx = className 
+ const Header = () =>(
+     <ul>
+     <link >Home</link>
+     <link to="/categories">Categories</link>
+     </ul>
+ )
+ 
