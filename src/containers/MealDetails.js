57f314477443fa5533990f762de8b29f67dbcdd8
dibdas <dibyandu08das@gmail.com>
2021-08-17 20:34:05 +0530

    modified

--- src/containers/MealDetails.js
+++ src/containers/MealDetails.js
@@ -1,8 +1,10 @@
 import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+
 import { connect } from 'react-redux';
-import { useEffect } from 'react';
+import classNames from 'classnames';
+import PropTypes from 'prop-types';
 import { fetchMealById } from '../redux/actions/thunk';
 import * as actionsType from '../redux/actions/actionTypes';
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
