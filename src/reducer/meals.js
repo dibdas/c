b7c186e70aab30adc6de751c7e40b745bd503450
dibdas <dibyandu08das@gmail.com>
2021-08-20 00:09:08 +0530

    added meal reducer

---
+++ src/reducer/meals.js
@@ -0,0 +1,26 @@
+
+import {ERROR_MEALS,SUCCESS_MEALS}from '../actions/constants'
+const defaultState={ meal:{},status:MEAL,error:null}
+
+const mealsreducer = (state= defaultState,action)=>{
+    switch(action.type){
+        case SUCCESS_MEALS:
+            return {
+                ...state,
+                status: SUCCESS_MEALS,
+                meal: action.data,
+                error: null
+            }
+            case ERROR_MEALS:
+            return {
+                ...state,
+                status: ERROR_MEALS,
+                error: action.error
+            }
+            default:{
+                return state
+            }
+    }
+}
+
+export default mealsreducer;
