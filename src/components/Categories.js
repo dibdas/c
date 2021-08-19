b84680e8a92e4515d2adad62a072effa2e33f244
dibdas <dibyandu08das@gmail.com>
2021-08-19 20:09:31 +0530

    modified

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,21 +1,19 @@
 import PropTypes from 'prop-types';
-import className from 'classnames';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import className from 'classnames';
 
 const sx = className 
 const Categories =({categories})=>(
-    <Link to={`/meals/${category.strCategory}`}>
-         <img src={categories.strCategoryThumb} />
+    <Link to={`/meals/${categories.strCategory}`}>
+         <img src={ categories.strCategoryThumb } />
          <h4>
          {categories.strCategory}
          </h4>
-
         </Link>
 
 )
 
 Categories.prototypes ={
-    meal:  PropTypes.objectOf(PropTypes.string).isRequired,
+    categories:  PropTypes.objectOf(PropTypes.string).isRequired,
 }
 export default Categories ;
