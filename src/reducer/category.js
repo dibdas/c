82abebece9876999b7d586fea40ac99e967849de
dibdas <dibyandu08das@gmail.com>
2021-08-20 00:08:46 +0530

    modified

---
+++ src/reducer/category.js
@@ -0,0 +1,11 @@
+import { CHANGE_CATEGORY } from "../actions/constants";
+const catergoryreducer=(state=[],action)=>{
+    switch (action.type){
+    case CHANGE_CATEGORY:
+        return state
+        default:
+        return state
+
+    }
+}
+export default catergoryreducer
