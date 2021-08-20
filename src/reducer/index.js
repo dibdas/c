6b01bfd5906e0b39b11c6ef48ca1bdf1b19496e9
dibdas <dibyandu08das@gmail.com>
2021-08-20 23:49:39 +0530

    modified

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,8 +2,8 @@
 import { combineReducers } from 'redux';
 import mealreducer from './meal';
 import mealsreducer from './meals';
-import catergoryreducer from './category';
+import categoryreducer from './category';
 
 
-const rootReducer = combineReducers({ mealreducer,mealsreducer,catergoryreducer});
+const rootReducer = combineReducers({ mealreducer,mealsreducer,categoryreducer});
 export default rootReducer;
