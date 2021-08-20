cbb3ed1ff1a2e45f526cafd9d1e8ffeb7d7b4ed6
dibdas <dibyandu08das@gmail.com>
2021-08-20 21:39:22 +0530

    added header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Categories from './components/Categoriesy';
 import MealDetails from './containers/MealDetails';
 import Meals from './containers/Meals';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <>
      <BrowserRouter>
+     < Header />
       <Route component={ Meals} exact path="/" />
       <Route component={Categories} exact path= "/categories" />
       <Route component={MealDetails} exact path = "/meal/:id" />
