2970e3343bfba5a040ac6ac126fffca91952e573
dibdas <dibyandu08das@gmail.com>
2021-08-23 01:24:16 +0530

    mod

--- src/components/test/Header.test.js
+++
@@ -1,9 +0,0 @@
-
-import { renderer } from '@testing-library/react';
-import Header from './Header.test';
-it('renders correctly', () => {
-  const tree = renderer
-    .create(<Header />)
-    .toJSON();
-  expect(tree).toMatchSnapshot();
-});
